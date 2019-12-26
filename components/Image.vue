a8c80659a866a8fcd363d66b8845c6688e2e6f63
Toke Reimer Skovgaard <toke@reimerskovgaard.dk>
2019-12-26 11:37:17 +0100

    Loading them images smartly

--- components/Image.vue
+++ components/Image.vue
@@ -9,12 +9,15 @@
         <img
                 data-sizes="auto"
                 :src="require(`~/assets/images/${src}?sqip`)"
-                :data-srcset="` ${require(`~/assets/images/${src}?resize&size=300`)} 300w,
+                :data-srcset="`
+                                ${require(`~/assets/images/${src}?webp?resize&size=600`)} 600w,
                                 ${require(`~/assets/images/${src}?resize&size=600`)} 600w,
-                                ${require(`~/assets/images/${src}?resize&size=900`)} 900w,
-                                ${require(`~/assets/images/${src}?resize&size=2000`)} 2000w,
-                                ${require(`~/assets/images/${src}?resize&size=4000`)} 4000w
+                                ${require(`~/assets/images/${src}?webp?resize&size=1200`)} 1200w,
+                                ${require(`~/assets/images/${src}?resize&size=1200`)} 1200w,
+                                ${require(`~/assets/images/${src}?webp?resize&size=2000`)} 2000w,
+                                ${require(`~/assets/images/${src}?resize&size=2000`)} 2000w
                                `"
+                :alt="alt"
                 class="lazyload"/>
     </figure>
 </template>
@@ -24,6 +27,11 @@
       src: {
         type: String,
         required: true
+      },
+      alt: {
+        type: String,
+        required: false,
+        default: "Image of something"
       }
     },
 
